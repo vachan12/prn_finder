4d2bdae8f60177fdcd49ed30ac71d61905022306
vachan12 <honnudike12@gmail.com>
2026-04-20 00:20:36 +0530

    bug fix

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 data_file = Path(__file__).parent / "data.json"
 with data_file.open("r", encoding="utf-8") as f:
